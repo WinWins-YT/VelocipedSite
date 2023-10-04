ee2724932db0f4b3aa11dcc1855ac8fa985f3862
WinWins <winwins@danimatcorp.com>
2023-10-05 01:42:31 +0300

    Added KB

--- VelocipedSite/ClientApp/src/components/Home.js
+++ VelocipedSite/ClientApp/src/components/Home.js
@@ -7,6 +7,8 @@ export class Home extends Component {
   render() {
         return (
             <div style={{justifyContent: "center"}} className={"row"}>
+                <h3 style={{textAlign: "center"}}>Добро пожаловать в службу доставки товаров из супермаркетов "Велосипед"</h3>
+                <p style={{textAlign: "center"}}>Выберите магазин из которого будет произведена доставка:</p>
                 <div className={"col-md-4 d-flex align-items-center shopBannerContainer"}>
                     <Link to={"/counter?shop=shesterochka"}>
                         <img className={"shopBanner"} src={"images/shet.jpg"} alt={""}/>
@@ -18,8 +20,8 @@ export class Home extends Component {
                     </Link>
                 </div>
                 <div className={"col-md-4 d-flex align-items-center shopBannerContainer"}>
-                    <Link to={"/counter?shop=fox"}>
-                        <img className={"shopBanner"} src={"images/fox.png"} alt={""}/>
+                    <Link to={"/counter?shop=kb"}>
+                        <img className={"shopBanner"} src={"images/logokb-2022.jpg"} alt={""}/>
                     </Link>
                 </div>
                 <div className={"col-md-4 d-flex align-items-center shopBannerContainer"}>
